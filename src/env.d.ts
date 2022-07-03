2a0e9adab23b4fbe4d3e3daf9699aeb6810706f4
sugtao4423 <sugtao4423@gmail.com>
2022-07-04 07:31:46 +0900

    各種URLを環境変数から取るように

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,12 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+interface ImportMetaEnv {
+  readonly VITE_GET_JSON_URL: string
+  readonly VITE_MEDIA_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- src/services/paths.ts
+++ src/services/paths.ts
@@ -1,6 +1,6 @@
-export const getJsonUrl = './getjson.php'
+export const getJsonUrl = import.meta.env.VITE_GET_JSON_URL
 
 export const createUserPath = (username: string) => `/${username}`
 
-export const createMediaPath = (username: string, mediaName: string) =>
-  `./data/${username}/${mediaName}`
+export const createMediaPath = (username: string, fileName: string) =>
+  `${import.meta.env.VITE_MEDIA_BASE_URL}/${username}/${fileName}`
