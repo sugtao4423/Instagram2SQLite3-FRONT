e351fe1aa4cc175c17307592ea31c2230ddf6c4d
sugtao4423 <sugtao4423@gmail.com>
2022-07-04 06:31:20 +0900

    画像と動画の基本コンポーネントを作成

---
+++ src/components/atoms/GraphVideo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ videoPath: string }>()
+</script>
+
+<template>
+  <div>
+    <video :src="videoPath" controls class="responsive-video" />
+  </div>
+</template>
