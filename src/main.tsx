d8ea9ac6f8e4e3d1cba78e805ccf1bd3ab942a96
sugtao4423 <sugtao4423@gmail.com>
2025-04-01 03:09:20 +0900

    Upgrade all libraries

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import Posts from './routes/posts'
 import Root from './routes/root'
 
 import './global.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <HashRouter>
       <Routes>
         <Route path="/" element={<Root />} />
         <Route path="/:userName" element={<Posts />} />
       </Routes>
     </HashRouter>
-  </React.StrictMode>
+  </StrictMode>
 )
